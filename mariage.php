313269c00392808d32096011d3f1af6011afcc89
Hissein-brahim <tourabybrahim@gmail.com>
2024-05-25 08:34:57 +0100

    mon premier commit

---
+++ mariage.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html xmlns:ff4j="http://www.w3.org/1999/xhtml"
+      xmlns:esi="http://www.edge-delivery.org/esi/1.0"
+      dir="ltr" data-fr-scheme="light"
+      data-fr-js="false" lang="fr">
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mariage</title>
+</head>
+<body>
+    <div class="fr-skiplinks">
+        <nav class="fr-container" role="navigation" aria-label="Acces rapide" lang="fr">
+            <ul class="fr-skiplinks__list">
+                <li>
+                    <a class="fr-link" href="#main">Contenu</a>
+                </li>
+                <li class="sp-display-lg">
+                    <a class="fr-link" href="#header-search-main">Recherche</a>
+                </li>
+                <li class="sp-display-lg">
+                    <a class="fr-link" href="##header-navigation">Menu</a>
+                </li>
+                <li><a class="fr-link" href="#footer">Pied de page</a></li>
+            </ul>
+        </nav>
+    </div>
+    <div class="fr-header" data-fr-js-header-actionne="true">
+        <div class="fr-header__navbar" id="header-search-mobile"><button type="button" class="fr-btn--search fr-btn" data-fr-opened="false" aria-controls="header-search" title="Ouvrir la recherche" id="fr-btn-header-search-mobile" data-fr-js-modal-button="true" data-fr-js-button-actionee="true" data-fr-js-header-modal-button-actionee="true">
+            Ouvrir la recherche
+          </button><button type="button" class="fr-btn--menu fr-btn sp-btn--menu" data-fr-opened="false" aria-controls="header-navigation" id="fr-btn-menu-mobile-3" data-fr-js-modal-button="true" data-fr-js-button-actionee="true" data-fr-js-header-modal-button-actionee="true">
+          Menu
+        </button></div>
+    </div>
+
+    <div class="fr-header__service">
+        <a href="https://www.service-public.fr/" title="Accueil Service-Public de la Mairie">
+        <p class="fr-header__service-title">Service-Public.fr</p></a>
+        <p class="fr-header__service-tagline">Le site officiel de la mairie au Cameroun</p>
+    </div>
+
+    <div class="fr-header__tools"><div class="fr-header__tools-links" data-fr-js-header-links="true"><ul class="fr-btns-group fr-links-group sp-links-group"><li data-test="profil"><a href="https://www.service-public.fr/compte/se-connecter" class="fr-btn fr-fi-user-fill fr-btn--icon-left" data-xiti-name="Mon_compte::Particuliers::Vos_droits::Mariage_en_France_resultat_recherche_guidee" data-xiti-type="action" data-fr-js-button-actionee="true" data-fr-js-header-tools-button-actionee="true"><span>Se connecter</span></a></li><li><a href="https://entreprendre.service-public.fr" class="fr-btn fr-displayed-lg sp-link-switch-site sp-text-color--red fr-fi-arrow-right-line fr-btn--icon-right" target="_blank" rel="noopener noreferrer" title="Accéder au site pour les entreprises - nouvelle fenêtre" data-test="lien-entreprendre" data-xiti-type="exit" data-xiti-name="Particuliers::Acces_entreprendre::Vos_droits::F930_Mariage_en_France_resultat_recherche_guidee" data-fr-js-button-actionee="true" data-fr-js-header-tools-button-actionee="true"><span>Accéder au site pour les entreprises</span></a></li></ul></div><div class="fr-header__search fr-modal" id="header-search" data-fr-js-modal="true" data-fr-js-header-modal="true" data-fr-js-header-modal-actionee="true" data-fr-js-modal-actionee="true" role="dialog" aria-labelledby="fr-btn-header-search-mobile"><div class="fr-container fr-container-lg--fluid"><button class="fr-link--close fr-link" aria-controls="header-search" title="Fermer la recherche" data-fr-js-modal-button="true" data-fr-js-header-modal-button-actionee="true" data-fr-js-link-actionee="true">Fermer</button><div class="sp-bloc-autocomplete" id="search"><form id="header-search-main" method="get" action="https://www.service-public.fr/particuliers/recherche" role="search" class="fr-search-bar sp-autocomplete select-a11y" aria-label="Tout le site" data-fr-js-search-actionee="true"><label class="fr-label" for="header-search-input">
+        Recherche
+      </label><div class="fr-sr-only sp-autocomplete-results" aria-live="polite"></div><p id="messageheader-search-input" class="fr-hidden Message">Des suggestions vous seront proposées lors de votre saisie situées après le champ</p><input class="fr-input" title="Rechercher sur le site" type="search" id="header-search-input" name="keyword" autocomplete="off" autocapitalize="off" spellcheck="false" value="" placeholder="Rechercher sur le site" maxlength="300" data-test="search-input" aria-describedby="messageheader-search-input" data-enableautocompletev2="true"><div class="sp-item-suggestions fr-hidden" data-test="sp-item-suggestions-container"><ul role="listbox"></ul></div><button class="fr-btn" type="submit" title="Rechercher sur le site" data-fr-js-button-actionee="true"><span>
+      Rechercher sur le site
+      </span></button></form></div></div></div></div>
+</body>
+</html>
